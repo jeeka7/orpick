f12a016228067479527f7ce0caa3d783576ac384
Dean Hunter <jeeka7@gmail.com>
2023-11-07 08:30:52 +0530

    Update orpick.py

--- orpick.py
+++ orpick.py
@@ -9,7 +9,7 @@ if st.sidebar.button("Student Registration"):
     st.write("Kindly fill this if you want to learn Music from Orpick Studios  \n[Student Registration Form](https://forms.gle/FAVrsh2XoCFrD4a78)")
 if st.sidebar.button("Event Inquiry"):
     st.title("Event inquiry")
-    st.write("Fill this form if you want to book Musicians for your next Event  \n[Event Inquiry](https://forms.gle/2uNpUCSdYJJS42f48)")
+    st.write("Fill this form if you want to book Musicians for your next Event  \n[Event Inquiry](https://forms.gle/7PuBR91XiMfswVNN9)")
 if st.sidebar.button("About Me"):
     st.title("About Me")
     st.write('Meet Iqbal Warsi, a maestro in the realm of music, whose passion for sonic artistry has led him to establish the renowned music studio, Orpick. With a rich musical background and an unwavering dedication to his craft, Iqbal has become a luminary figure in the industry.   \n\nFor the past 15 years, he has not only honed his own musical prowess but has also imparted his knowledge and expertise to countless aspiring musicians. As the proud owner of Orpick, Iqbal provides a nurturing space for creativity to flourish. His commitment to cultivating talent is reflected in the success stories of the students who have passed through the doors of his studio, making Orpick a beacon for those seeking to master the harmonies of the musical world.')
